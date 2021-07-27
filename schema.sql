0075f03c9ddad3ee1a783188e17cd28d8ae288b6
J-tok <83428759+J-tok@users.noreply.github.com>
2021-07-27 18:51:11 -0500

    Committing Scheme code for PGAdmin SQL Table buidling

---
+++ schema.sql
@@ -0,0 +1,58 @@
+-- Creaing Tables for PH-EmployeeDB (Departments)
+Create Table departments (
+	dept_no VARCHAR(4) NOT NULL,
+	dept_name VARCHAR(40) NOT NULL,
+	PRIMARY KEY (dept_no),
+	UNIQUE (dept_name)
+);
+
+-- Creating Table for Employees
+Create Table employees (
+	emp_no INT NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	gender VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	PRIMARY KEY (emp_no)
+	);
+-- Creating Department Manager Table	
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+-- Creating Salaries Table
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+--Creating Department Employee Table
+Create TABLE dept_emp (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+		PRIMARY KEY (emp_no, dept_no)
+);
+--Creating Titles Table
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY(emp_no,title,from_date)
+);
+
+SELECT * FROM departments;
